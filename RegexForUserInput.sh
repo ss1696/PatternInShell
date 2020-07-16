1d3787666ce8c56e51f42976966264857bd9be0d
shashank srivastava <shashank.1696@gmail.com>
2020-07-17 03:33:49 +0530

    Validating Mobile No.

--- RegexForUserInput.sh
+++ RegexForUserInput.sh
@@ -9,6 +9,9 @@ read firstName
 echo "Enter Your Last Name"
 read lastName
 
+echo "Enter Your Mobile No."
+read mobileNo
+
 patFirstName="^[A-Z]{1}[A-Z a-z]{3,}$"
 
 if [[ $firstName =~ $patFirstName ]]
@@ -25,5 +28,14 @@ then
         echo "Valid Last Name"
 else
         echo "Invalid Last Name"
+fi
+
+patMobileNo="^[\+0-9]{2,3}[ ]?[0-9]{10}$"
+
+if [[ $mobileNo =~ $patMobileNo ]]
+then
+        echo "Valid Mobile No."
+else
+        echo "Invalid Mobile No."
 fi
 
